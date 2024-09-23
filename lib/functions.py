46d9aed3aa4b9a16a6e25cf4103e42d0fa590c22
joycewangui <joycewangui590@gmail.com>
2024-09-23 17:22:42 +0300

    adds unction

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,20 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name= "programmer"):
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    num1 = 45
+    num2 = 55
+    return num1 + num2
+    
+
 
 def halve(number):
-    pass
+    return number/2
